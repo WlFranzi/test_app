6f5b1081eb134fe377434c22cd41217d0d41e69a
fwittleder <franziska.wittleder@gmail.com>
2015-01-21 18:24:21 +0100

    about

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141023131330) do
+ActiveRecord::Schema.define(version: 20150121171450) do
 
   create_table "products", force: true do |t|
     t.string   "hersteller"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141023131330) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "street"
+    t.float    "rating"
   end
 
 end
